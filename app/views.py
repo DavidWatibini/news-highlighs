b707bbebb49bf39c2a2022d9cc529f4d8b4ed9fe
Watinguyaz <watimakhanu@gmail.com>
2019-02-04 05:26:35 +0300

    views updated together with relevant html files

--- app/views.py
+++ app/views.py
@@ -7,12 +7,12 @@ def index():
 
     return render_template('index.html')
 
-@app.route('/news-article')
-def index():
+@app.route('/newsArticle')
+def newsArticle():
 
-    return render_template('news-article.html')
+    return render_template('newsArticle.html')
 
-@app.route('/news-source')
-def index():
+@app.route('/newsSource')
+def newsSource():
 
-    return render_template('news-source.html')
+    return render_template('newsSource.html')
